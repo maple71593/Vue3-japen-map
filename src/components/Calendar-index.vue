e5aa39d9f9a2d0c55404554ea2fd3147fca01f38
maple71593 <131730147+maple71593@users.noreply.github.com>
2023-10-16 12:26:47 +0800

    版本更新 完成了前選值不能大於後選值的判斷 持續更新

--- src/components/Calendar-index.vue
+++ src/components/Calendar-index.vue
@@ -1,6 +1,6 @@
 <script setup>
-// import { getTime } from '@/api/home.js'
 import { ref } from 'vue'
+
 // 全部演算數據的儲存
 const DateArrayRef = ref({})
 //獲取現在時間 年 月 日
@@ -15,7 +15,7 @@ const GetDate = (Year, Month, index) => {
   //取得下個月的回推這個月的最後一天(獲得當月總共有幾天)
   const GetMonLastDay = new Date(Year, Month, 0).getDate()
   // 新增日期資料為空共計6個禮拜42天
-  const DateArray = Array.from({ length: 42 }, (_, index) => [''])
+  const DateArray = Array.from({ length: 42 }, (_, index) => [index])
   // 將為空的資料儲存到Ref
   DateArrayRef.value[index] = DateArray
   // 將數據從每個月的第一天開始導入 遍歷推進ref
@@ -34,7 +34,7 @@ for (let i = 0; i <= 12; i++) {
   let month = (Month + i) % 13
   // 取得年分
   let year = Year
-  // 判斷是否跨份
+  // 判斷是否跨年份
   if (month < Month) {
     year = year + 1
     month += 1
@@ -42,25 +42,77 @@ for (let i = 0; i <= 12; i++) {
   //呼叫函數 並回傳參數
   GetDate(year, month, i)
 }
-const getdata = (data) => {
-  console.log(CharacterData)
+
+//改變月曆輪播的方向
+const left = ref(0)
+const ChangeLeft = (num) => {
+  if (left.value === 0 && num === 435) return
+  if (left.value === -4785 && num === -435) return
+  const newlaft = left.value + num
+  left.value = newlaft
 }
-getdata()
+//點擊改變input裡數值
+const InpRefStart = ref()
+const InpRefStartData = ref()
+const InpRefEnd = ref()
+const InpRefEndData = ref()
+const StartChecked = ref(false)
+const EndChecked = ref(false)
+const GetData = (item) => {
+  const { year, mon, date } = item
+  if (year === undefined && mon === undefined && date === undefined) return
+  const checkedDay = [year, mon, date].join('/')
+  const checkedData = `${year}${mon.toString().padStart(2, '0')}${date
+    .toString()
+    .padStart(2, '0')}`
+
+  if (StartChecked.value === true) {
+    if (EndChecked.value === true) {
+      if (InpRefStartData.value > checkedData) {
+        alert('出發日期不能大於結束日期')
+      } else {
+        InpRefEnd.value = checkedDay
+        InpRefEndData.value = checkedData
+      }
+    } else {
+      InpRefEnd.value = checkedDay
+      InpRefEndData.value = checkedData
+      EndChecked.value = true
+    }
+  } else {
+    if (StartChecked.value === false && EndChecked.value === false)
+      InpRefStart.value = checkedDay
+    console.log(StartChecked.value)
+    InpRefStartData.value = checkedData
+    StartChecked.value = true
+  }
+}
 </script>
 <template>
   <div>
     <input
+      v-model="InpRefStart"
       class="inputTest"
       type="text"
       placeholder="出發日期 年/月/日"
-    /><input class="inputTest" placeholder="結束日期 年/月/日" type="text" />
+    /><input
+      v-model="InpRefEnd"
+      class="inputTest"
+      placeholder="結束日期 年/月/日"
+      type="text"
+    />
   </div>
-  <div class="btn">
-    <button class="btnup">上一個</button>
-    <button class="btndon">下一個</button>
+  <div class="btn-Relative">
+    <button class="btnLeft" @click="ChangeLeft(435)">〈</button>
+    <button class="btnRight" @click="ChangeLeft(-435)">〉</button>
   </div>
   <div class="calenderCOM">
-    <div class="calender" v-for="(items, indexs) in DateArrayRef" :key="indexs">
+    <div
+      class="calender"
+      :style="{ left: left + 'px' }"
+      v-for="(items, indexs) in DateArrayRef"
+      :key="indexs"
+    >
       <div class="vforbox">
         <h2>{{ items[15].year }}年{{ items[15].mon }}月</h2>
         <table class="tablebox">
@@ -88,8 +140,13 @@ getdata()
             </tr>
           </thead>
           <tbody class="ManthBox">
-            <tr class="datebox" v-for="(item, index) in items" :key="index">
-              <td @click="getdata(item)">{{ item.date }}</td>
+            <tr
+              @click="GetData(item)"
+              class="datebox"
+              v-for="(item, index) in items"
+              :key="index"
+            >
+              <td>{{ item.date }}</td>
             </tr>
           </tbody>
         </table>
@@ -99,46 +156,40 @@ getdata()
 </template>
 
 <style>
+/* 處理input */
 .inputTest {
   box-sizing: border-box;
   padding-left: 10px;
   margin-left: 10px;
   margin-bottom: 10px;
-  width: 420px;
+  width: 425px;
   height: 40px;
   font-size: 20px;
   border-radius: 10px;
 }
-
+/* 月曆外圍(可顯現範圍) */
 .calenderCOM {
   width: 870px;
+  height: 260px;
+  margin-left: 10px;
   overflow: hidden;
   display: flex;
+}
+.calender {
+  top: 0px;
+  left: 0px;
+  transition: 0.2s ease;
   position: relative;
 }
-.btn1 {
-  position: relative;
-}
-.btnup {
-  top: 40%;
-  left: -1%;
-  width: 40px;
-  height: 60px;
-  background-color: aqua;
-  position: absolute;
-}
-.btndon {
-  width: 40px;
-  height: 60px;
-  background-color: rgb(46, 46, 46);
-  position: absolute;
-}
+/* 月曆範圍(內部) */
 .vforbox {
+  width: 425px;
   text-align: center;
   border-radius: 10px;
-  margin-left: 10px;
+  margin-right: 10px;
   background-color: #f37e7e;
   color: rgb(255, 255, 255);
+  box-sizing: border-box;
 }
 .vforbox h2 {
   line-height: 50px;
@@ -163,5 +214,57 @@ getdata()
   margin: auto;
   border-radius: 10px;
   cursor: pointer;
+}
+/* 輪播圖方向按鈕 */
+.btn-Relative {
+  position: relative;
+}
+.btnLeft {
+  width: 50px;
+  height: 60px;
+  font-size: 30px;
+  padding-right: 10px;
+  background-color: #3498db;
+  color: #fff;
+  text-align: center;
+  text-transform: uppercase;
+  text-decoration: none;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+  top: 100px;
+  left: -60px;
+  position: absolute;
+}
+
+.btnLeft:hover {
+  background-color: #1e87d7;
+  transition: background-color 0.3s ease;
+}
+
+.btnRight {
+  width: 50px;
+  height: 60px;
+  font-size: 30px;
+  padding-left: 10px;
+  background-color: #3498db;
+  color: #fff;
+  text-align: center;
+  text-transform: uppercase;
+  text-decoration: none;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+  top: 100px;
+  right: -60px;
+  position: absolute;
+}
+.btnRight:hover {
+  background-color: #1e87d7;
+  transition: background-color 0.3s ease;
 }
 </style>
